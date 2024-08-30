1876f28fc029774c8ba55f62719e9efa1ce6e1ab
C <157691477+caellex@users.noreply.github.com>
2024-08-31 00:00:49 +0200

    Testing redirects and a multi page setup with Node

---
+++ server.js
@@ -0,0 +1,43 @@
+const sql = require("msnodesqlv8");
+
+const http = require('http');
+const fs = require('fs')
+
+const connectionString = "server=(localdb)\\Mock;Database=FullTest;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
+
+const server = http.createServer((req, res) => {
+    console.log('request received');
+    console.log(req.url,"\n", req.method)
+
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views/'
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+
+    //send a file
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err)
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+            
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on port 3000')
+})
